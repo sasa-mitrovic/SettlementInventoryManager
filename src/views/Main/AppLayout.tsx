9a76e46f66e18ce323c89215bcaf523eaae33233
Sasa <sasam1996@gmail.com>
2025-07-23 19:11:47 -0400

    Fix Permissions issue, DB refreshed

--- src/views/Main/AppLayout.tsx
+++ src/views/Main/AppLayout.tsx
@@ -1,10 +1,12 @@
 import { Box, Group, Stack } from '@mantine/core';
 import { Outlet } from 'react-router-dom';
 import { DarkModeToggle } from '../../components/DarkModeToggle';
+import { ImpersonationBanner } from '../../components/ImpersonationBanner';
 
 export const AppLayout = () => {
   return (
-    <Stack>
+    <Stack gap={0}>
+      <ImpersonationBanner />
       <Group justify="flex-end" p="md">
         <DarkModeToggle />
       </Group>
